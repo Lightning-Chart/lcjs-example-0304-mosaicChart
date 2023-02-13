52f9a3dc4ec91b19958aa4bb2aa0d5c3ebc5da8f
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2023-02-13 13:47:36 +0200

    LCJS 4.0.1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # JavaScript Mosaic Chart
 
-![JavaScript Mosaic Chart](mosaicChart.png)
+![JavaScript Mosaic Chart](mosaicChart-darkGold.png)
 
 This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
 
@@ -8,18 +8,18 @@ LightningChart JS is entirely GPU accelerated and performance optimized charting
 
 The demo can be used as an example or a seed project. Local execution requires the following steps:
 
-- Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
-- Open the project folder in a terminal:
+-   Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+-   Open the project folder in a terminal:
 
-        npm install              # fetches dependencies
-        npm start                # builds an application and starts the development server
+          npm install              # fetches dependencies
+          npm start                # builds an application and starts the development server
 
-- The application is available at *http://localhost:8080* in your browser, webpack-dev-server provides hot reload functionality.
+-   The application is available at _http://localhost:8080_ in your browser, webpack-dev-server provides hot reload functionality.
 
 
 ## Description
 
-*Also known as Marimekko Chart*
+_Also known as Marimekko Chart_
 
 This example shows creation of a Mosaic Chart made on user side by utilizing RectangleSeries. Mosaic Chart can be used to compare observation counts of groups (sub-categories) across different categories.
 
@@ -38,12 +38,8 @@ const category2 = chart.addCategory('Category #2')
 category1.setCategoryValue(60)
 category2.setCategoryValue(40)
 // Set values of sub-categories on categories (relative Y size).
-category1
-    .setSubCategoryValue(A, 40)
-    .setSubCategoryValue(B, 60)
-category2
-    .setSubCategoryValue(A, 80)
-    .setSubCategoryValue(B, 20)
+category1.setSubCategoryValue(A, 40).setSubCategoryValue(B, 60)
+category2.setSubCategoryValue(A, 80).setSubCategoryValue(B, 20)
 ```
 
 The actual Mosaic Chart logic just serves to provide a starting point for users to create their own API based on their needs and preferences.
@@ -67,31 +63,31 @@ The actual Mosaic Chart logic just serves to provide a starting point for users 
 
 If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
 
-Official [API documentation][1] can be found on [Arction][2] website.
+Official [API documentation][1] can be found on [LightningChart][2] website.
 
 If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
 
-If you think you found a bug in the LightningChart JavaScript library, please contact support@arction.com.
+If you think you found a bug in the LightningChart JavaScript library, please contact support@lightningchart.com.
 
-Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@arction.com.
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@lightningchart.com.
 
 [0]: https://github.com/Arction/
-[1]: https://www.arction.com/lightningchart-js-api-documentation/
-[2]: https://www.arction.com
+[1]: https://lightningchart.com/lightningchart-js-api-documentation/
+[2]: https://lightningchart.com
 [3]: https://stackoverflow.com/questions/tagged/lightningchart
-[4]: https://www.arction.com/support-services/
+[4]: https://lightningchart.com/support-services/
 
-© Arction Ltd 2009-2020. All rights reserved.
+© LightningChart Ltd 2009-2022. All rights reserved.
 
 
-[XY cartesian chart]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/chartxy.html
-[Rectangle series]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/rectangleseries.html
-[Axis]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/axis.html
-[Solid FillStyle]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/solidfill.html
-[Empty FillStyle]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/globals.html#emptyfill
-[Solid LineStyle]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/solidline.html
-[Empty LineStyle]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/globals.html#emptyline
-[Empty TickStyle]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/globals.html#emptytick
-[RGBA color factory]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/globals.html#colorrgba
-[UI position origins]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/globals.html#uiorigins
+[XY cartesian chart]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/ChartXY.html
+[Rectangle series]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/RectangleSeries.html
+[Axis]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/Axis.html
+[Solid FillStyle]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/SolidFill.html
+[Empty FillStyle]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/variables/emptyFill-1.html
+[Solid LineStyle]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/SolidLine.html
+[Empty LineStyle]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/variables/emptyLine.html
+[Empty TickStyle]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/variables/emptyTick.html
+[RGBA color factory]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/functions/ColorRGBA.html
+[UI position origins]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/variables/UIOrigins.html
 
--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const {
     UIBackgrounds,
     UIOrigins,
     AxisTickStrategies,
-    Themes
+    Themes,
 } = lcjs
 
 const lc = lightningChart()
@@ -26,61 +26,59 @@ let mosaicChart
 {
     mosaicChart = () => {
         // Create a XY-Chart and add a RectSeries to it for rendering rectangles.
-        const chart = lc.ChartXY({
-            // theme: Themes.darkGold 
-        })
+        const chart = lc
+            .ChartXY({
+                // theme: Themes.darkGold
+            })
             .setTitle('Controlled Group Testing')
             .setMouseInteractions(false)
             // Disable default AutoCursor
             .setAutoCursorMode(0)
         const rectangles = chart.addRectangleSeries()
 
-        const bottomAxis = chart.getDefaultAxisX()
-            .setInterval(0, 100)
+        const bottomAxis = chart
+            .getDefaultAxisX()
+            .setInterval({ start: 0, end: 100, stopAxisAfter: false })
             .setScrollStrategy(undefined)
             .setMouseInteractions(false)
             .setTitle('%')
-        const leftAxis = chart.getDefaultAxisY()
-            .setInterval(0, 100)
+        const leftAxis = chart
+            .getDefaultAxisY()
+            .setInterval({ start: 0, end: 100, stopAxisAfter: false })
             .setMouseInteractions(false)
             // Hide default ticks of left Axis.
             .setTickStrategy(AxisTickStrategies.Empty)
-        const rightAxis = chart.addAxisY({ opposite: true })
-            .setInterval(0, 100)
+        const rightAxis = chart
+            .addAxisY({ opposite: true })
+            .setInterval({ start: 0, end: 100, stopAxisAfter: false })
             .setScrollStrategy(undefined)
             .setMouseInteractions(false)
             .setTitle('%')
-        const topAxis = chart.addAxisX({ opposite: true })
-            .setInterval(0, 100)
+        const topAxis = chart
+            .addAxisX({ opposite: true })
+            .setInterval({ start: 0, end: 100, stopAxisAfter: false })
             .setMouseInteractions(false)
             // Hide default ticks of top Axis.
             .setTickStrategy(AxisTickStrategies.Empty)
 
         // Create marker for the top of each column.
-        const categoryMarkerBuilder = UIElementBuilders.AxisTick
-            // Style the marker.
-            .addStyler((marker) => marker
-                .setTextFillStyle(new SolidFill({ color: ColorRGBA(170, 170, 170) }))
-            )
+        const categoryMarkerBuilder = UIElementBuilders.AxisTickMajor
         // Create text on top of each section.
         const subCategoryLabelBuilder = UIElementBuilders.TextBox
             // Style the label.
-            .addStyler(label => label
-                // Set the origin point and fillStyle (color) for the label.
-                .setOrigin(UIOrigins.Center)
-                .setTextFillStyle(new SolidFill().setColor(ColorRGBA(255, 255, 255)))
-                .setMouseInteractions(false)
-                .setBackground((background) => background
-                    .setFillStyle(emptyFill)
-                    .setStrokeStyle(emptyLine)
-                )
+            .addStyler((label) =>
+                label
+                    // Set the origin point and fillStyle (color) for the label.
+                    .setOrigin(UIOrigins.Center)
+                    .setTextFillStyle(new SolidFill().setColor(ColorRGBA(255, 255, 255)))
+                    .setMouseInteractions(false)
+                    .setBackground((background) => background.setFillStyle(emptyFill).setStrokeStyle(emptyLine)),
             )
 
         const categories = []
         const yCategories = []
         const subCategories = []
         let margin = 0.1
-
 
         // Recreate rectangle figures from scratch.
         const _updateChart = () => {
@@ -93,52 +91,55 @@ let mosaicChart
                 // For each category on a single column, recreate the marker to the left of the chart.
                 for (const yCategory of yCategories) {
                     yCategory.tick
-                        .setTextFormatter(_ => yCategory.name)
+                        .setTextFormatter((_) => yCategory.name)
                         .setValue(yCategory.value)
-                        .restoreMarker()
+                        .setMarkerVisible(true)
                 }
                 // For each category (or column)
                 for (const category of categories) {
                     // Calculate the correct value to display for each category
-                    const relativeCategoryValue = 100 * category.value / sumCategoryValues
+                    const relativeCategoryValue = (100 * category.value) / sumCategoryValues
                     const sumSubCategoryValues = category.subCategories.reduce((prev, cur) => prev + cur.value, 0)
                     // If there are subCategories for the column
                     if (sumSubCategoryValues > 0) {
                         // Recreate the tick to display above each category and set the correct value to it
                         category.tick
-                            .setTextFormatter(_ => category.name + " (" + Math.round(relativeCategoryValue) + "%)")
+                            .setTextFormatter((_) => category.name + ' (' + Math.round(relativeCategoryValue) + '%)')
                             .setValue(xPos + relativeCategoryValue / 2)
-                            .restoreMarker()
+                            .setMarkerVisible(true)
                         let yPos = 0
                         for (const subCategory of category.subCategories) {
                             // Calculate proper value for the subCategory
-                            const relativeSubCategoryValue = 100 * subCategory.value / sumSubCategoryValues
+                            const relativeSubCategoryValue = (100 * subCategory.value) / sumSubCategoryValues
                             if (relativeSubCategoryValue > 0) {
                                 const rectangleDimensions = {
                                     x: xPos + margin,
                                     y: yPos + margin,
                                     width: relativeCategoryValue - 2 * margin,
-                                    height: relativeSubCategoryValue - 2 * margin
+                                    height: relativeSubCategoryValue - 2 * margin,
                                 }
                                 // Create a rectangle to represent the subCategory
-                                rectangles.add(rectangleDimensions)
+                                rectangles
+                                    .add(rectangleDimensions)
                                     .setFillStyle(subCategory.subCategory.fillStyle)
                                     .setStrokeStyle(emptyLine)
                                 // Recreate the label for the subCategory and update the value for it
                                 subCategory.label
-                                    .setText(Math.round(relativeSubCategoryValue) + "%")
-                                    .setPosition({ x: xPos + relativeCategoryValue / 2, y: yPos + relativeSubCategoryValue / 2 })
-                                    .restore()
-                            } else
-                                // The subCategory is not shown, so we can dispose of its label.
-                                subCategory.label.dispose()
+                                    .setText(Math.round(relativeSubCategoryValue) + '%')
+                                    .setPosition({
+                                        x: xPos + relativeCategoryValue / 2,
+                                        y: yPos + relativeSubCategoryValue / 2,
+                                    })
+                                    .setVisible(true)
+                            }
+                            // The subCategory is not shown, so we can dispose of its label.
+                            else subCategory.label.setVisible(false)
                             yPos += relativeSubCategoryValue
                         }
                     } else {
                         // There are no subCategories for the column, so the elements related to it can be disposed.
-                        category.tick
-                            .disposeMarker()
-                        category.subCategories.forEach(sub => sub.label.dispose())
+                        category.tick.setMarkerVisible(false)
+                        category.subCategories.forEach((sub) => sub.label.setVisible(false))
                     }
                     xPos += relativeCategoryValue
                 }
@@ -153,7 +154,7 @@ let mosaicChart
                     // Refresh the chart.
                     _updateChart()
                     return this
-                }
+                },
             }
             subCategories.push(subCategory)
             return subCategory
@@ -163,9 +164,7 @@ let mosaicChart
             const category = {
                 name,
                 value: 0,
-                tick: topAxis.addCustomTick(categoryMarkerBuilder)
-                    .setGridStrokeStyle(emptyLine)
-                ,
+                tick: topAxis.addCustomTick(categoryMarkerBuilder).setGridStrokeStyle(emptyLine),
                 subCategories: [],
                 setCategoryValue(value) {
                     this.value = value
@@ -173,19 +172,22 @@ let mosaicChart
                     return this
                 },
                 setSubCategoryValue(subCategory, value) {
-                    const existing = this.subCategories.find(a => a.subCategory == subCategory)
+                    const existing = this.subCategories.find((a) => a.subCategory == subCategory)
                     if (existing !== undefined) {
                         existing.value = value
                     } else {
                         this.subCategories.push({
                             subCategory,
                             value,
-                            label: chart.addUIElement(subCategoryLabelBuilder, { x: bottomAxis, y: rightAxis })
+                            label: chart.addUIElement(subCategoryLabelBuilder, {
+                                x: bottomAxis,
+                                y: rightAxis,
+                            }),
                         })
                     }
                     _updateChart()
                     return this
-                }
+                },
             }
             categories.push(category)
             return category
@@ -195,13 +197,12 @@ let mosaicChart
             const yCategory = {
                 name,
                 value: value,
-                tick: leftAxis.addCustomTick(categoryMarkerBuilder)
-                    .setGridStrokeStyle(emptyLine),
+                tick: leftAxis.addCustomTick(categoryMarkerBuilder).setGridStrokeStyle(emptyLine),
                 setCategoryYValue(value) {
                     this.value = value
                     _updateChart()
                     return this
-                }
+                },
             }
             yCategories.push(yCategory)
             return yCategory
@@ -210,37 +211,37 @@ let mosaicChart
         return {
             addSubCategory,
             addCategory,
-            addYCategory
+            addYCategory,
         }
     }
 }
 
 // Use the interface for example.
 const chart = mosaicChart()
-chart.addYCategory("Refreshed", 80)
-chart.addYCategory("No Effect", 40)
-chart.addYCategory("Caused Exhaustion", 12)
+chart.addYCategory('Refreshed', 80)
+chart.addYCategory('No Effect', 40)
+chart.addYCategory('Caused Exhaustion', 12)
 
-const subCategory_exhaust = chart.addSubCategory()
-    .setFillStyle(new SolidFill().setColor(ColorRGBA(200, 0, 0)))
-const subCategory_noEffect = chart.addSubCategory()
-    .setFillStyle(new SolidFill().setColor(ColorRGBA(240, 190, 0)))
-const subCategory_refresh = chart.addSubCategory()
-    .setFillStyle(new SolidFill().setColor(ColorRGBA(0, 180, 0)))
+const subCategory_exhaust = chart.addSubCategory().setFillStyle(new SolidFill().setColor(ColorRGBA(200, 0, 0)))
+const subCategory_noEffect = chart.addSubCategory().setFillStyle(new SolidFill().setColor(ColorRGBA(240, 190, 0)))
+const subCategory_refresh = chart.addSubCategory().setFillStyle(new SolidFill().setColor(ColorRGBA(0, 180, 0)))
 
-chart.addCategory('With caffeine')
+chart
+    .addCategory('With caffeine')
     .setCategoryValue(48)
     .setSubCategoryValue(subCategory_exhaust, 25)
     .setSubCategoryValue(subCategory_noEffect, 35)
     .setSubCategoryValue(subCategory_refresh, 40)
 
-chart.addCategory('Decaffeinated')
+chart
+    .addCategory('Decaffeinated')
     .setCategoryValue(32)
     .setSubCategoryValue(subCategory_exhaust, 10)
     .setSubCategoryValue(subCategory_noEffect, 45)
     .setSubCategoryValue(subCategory_refresh, 45)
 
-chart.addCategory('Placebo product')
+chart
+    .addCategory('Placebo product')
     .setCategoryValue(20)
     .setSubCategoryValue(subCategory_exhaust, 20)
     .setSubCategoryValue(subCategory_noEffect, 50)
